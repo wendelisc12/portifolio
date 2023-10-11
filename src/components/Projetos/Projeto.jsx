6f94c5dd258fea43b2dff65060b61331dc15125f
wendel <wendel.isaac12@gmail.com>
2023-10-11 01:26:22 -0300

    fix: correção do css responsivo

--- src/components/Projetos/Projeto.jsx
+++ src/components/Projetos/Projeto.jsx
@@ -4,7 +4,7 @@ const Projeto = ({thumbnail, titulo, tecnologias,linkProjeto,linkCodigo, thumbna
     return ( 
         <div id="main-projeto">
             <div id="image-projeto">
-                <div id='image-site' style={{backgroundImage: `url(${thumbnail})`,backgroundSize:'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center',width: '470px', height: '270px'}}>
+                <div id='image-site' style={{backgroundImage: `url(${thumbnail})`,backgroundSize:'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
                     <div id='image-celular'style={{backgroundImage: `url(${thumbnailCelular})`,backgroundSize:'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div>
                 </div>
             </div>
